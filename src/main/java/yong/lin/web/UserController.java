03a77008a30c58a4061268c58439880cf589b4cc
bearlin <xuyonglin0920@gmail.com>
2018-10-28 19:12:56 +0800

    Vue项目

--- src/main/java/yong/lin/web/UserController.java
+++ src/main/java/yong/lin/web/UserController.java
@@ -2,10 +2,7 @@ package yong.lin.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import yong.lin.domain.User;
 import yong.lin.service.UserService;
 
@@ -14,6 +11,8 @@ import java.util.List;
 @Controller
 @RequestMapping("/user")
 @ResponseBody
+
+//@RestController  等于一个   controller 和 response
 public class UserController {
 
     @Autowired
